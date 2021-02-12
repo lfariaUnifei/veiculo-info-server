42254792079f003da327456c3598b2a281e189ee
Luã Faria <lfaria@localhost.localdomain>
2021-02-12 09:13:50 -0300

    feat: segregating routes

--- src/modules/vehicle/routes/routes.ts
+++ src/modules/vehicle/routes/routes.ts
@@ -1,10 +1,18 @@
-import { Router } from '../../../../../shared/utils/router';
+import { Router } from '../../../shared/utils/router';
 import { ListVehicleController } from '../controllers/list-vehicle.controller';
+import { CreateVehicleController } from '../controllers/create-vehicle.controller';
+import { VehicleRepository } from '../repositories/vehicle.repository';
 
 export default function registerRoutes(): void {
   const router = Router.getInstance();
+  const vehicleRepository = new VehicleRepository();
   router.get({
     path: '/vehicle',
-    handler: new ListVehicleController(),
+    handler: new ListVehicleController(vehicleRepository),
+  });
+
+  router.post({
+    path: '/vehicle',
+    handler: new CreateVehicleController(vehicleRepository),
   });
 }
---
+++ src/shared/infra/http/routes/all-routes.ts
@@ -0,0 +1,5 @@
+import registerVehicleRoutes from '../../../../modules/vehicle/routes/routes';
+
+export default function registerAllRoutes(): void {
+  registerVehicleRoutes();
+}
